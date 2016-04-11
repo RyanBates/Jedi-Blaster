a656c87221aac1e805031a88e8f9bd81c5fec996
RYBates <ryan.bates@students.aie.edu.au>
2016-04-11 13:01:25 -0500

    the first steps to the game

---
+++ Assets/Camera_Control.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_Control : MonoBehaviour
+{
+    public Transform target;
+    public int Distance = 5;
+    public int height = 10;
+    public bool follow = true;
+
+    void Update()
+    {
+        Vector3 WantedPosition;
+
+        if (follow == true)
+            WantedPosition = new Vector3(0,height, -Distance);
+        
+        else
+            WantedPosition = new Vector3(0, height, Distance);
+       
+    }
+}
---
+++ Assets/Tank_Movement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Tank_Movement : MonoBehaviour
+{
+    public float TopSpeed = 4;
+
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical") * TopSpeed;
+        float h = Input.GetAxis("Horizontal") * TopSpeed;
+
+        gameObject.transform.position += transform.forward * (v * (.9f * Time.deltaTime));
+        
+        gameObject.transform.localPosition = new Vector3(transform.localPosition.x,0f,transform.localPosition.z);
+    }
+}
